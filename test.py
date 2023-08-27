151fb12fedc2f7bb6a5a2ebc84fa1a738b0a833a
Joni Pelham <jonititan@gmail.com>
2023-08-27 14:22:15 +0100

    updates to image and test

--- test.py
+++ test.py
@@ -3,12 +3,18 @@ import geojson
 import geopandas
 import overpy
 import movingpandas
+import prefect
+from prefect import flow, task
+from typing import List
+import httpx
 
 print('Geopy version:{}'.format(geopy.__version__))
 print('Geojson version:{}'.format(geojson.__version__))
 print('Geopandas version:{}'.format(geopandas.__version__))
 print('Overpy version:{}'.format(overpy.__version__))
 print('Movingpandas version:{}'.format(movingpandas.__version__))
+print('Prefect version:{}'.format(prefect.__version__))
+
 
 from shapely.geometry import Point
 d = {'col1': ['name1', 'name2'], 'geometry': [Point(1, 2), Point(2, 1)]}
@@ -23,3 +29,18 @@ from geopy.distance import geodesic
 print('Distance between the two points is:{}'.format(geodesic(gdf.loc[0]['geometry'].coords,gdf.loc[1]['geometry'].coords).miles))
 
 
+@task(retries=3)
+def get_stars(repo: str):
+    url = f"https://api.github.com/repos/{repo}"
+    count = httpx.get(url).json()["stargazers_count"]
+    print(f"{repo} has {count} stars!")
+
+
+@flow(name="GitHub Stars")
+def github_stars(repos: List[str]):
+    for repo in repos:
+        get_stars(repo)
+
+
+# run the flow!
+github_stars(["PrefectHQ/Prefect"])
