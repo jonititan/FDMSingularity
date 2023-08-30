608782995b1886355592c852040272e8086acc11
Joni Pelham <jonititan@gmail.com>
2023-08-30 15:54:49 +0100

    updated to include gpu test when --nv flag used

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ import prefect
 from prefect import flow, task
 from typing import List
 import httpx
+import numba
+import dask_cuda 
+import os
+for key,val in dict(os.environ).items():
+  print('Variable:{}  Value:{}'.format(key,val))
 
 print('Geopy version:{}'.format(geopy.__version__))
 print('Geojson version:{}'.format(geojson.__version__))
@@ -14,7 +19,15 @@ print('Geopandas version:{}'.format(geopandas.__version__))
 print('Overpy version:{}'.format(overpy.__version__))
 print('Movingpandas version:{}'.format(movingpandas.__version__))
 print('Prefect version:{}'.format(prefect.__version__))
+print('Numba version:{}'.format(numba.__version__))
+print('Dask GPU version:{}'.format(dask_cuda.__version__))
 
+from numba import cuda
+cuda.select_device(0)
+dev = cuda.current_context().device
+# prints e.g. "GPU-e6489c45-5b68-3b03-bab7-0e7c8e809643"
+print(dev.uuid)
+cuda.close()
 
 from shapely.geometry import Point
 d = {'col1': ['name1', 'name2'], 'geometry': [Point(1, 2), Point(2, 1)]}
@@ -27,6 +40,7 @@ gdf.to_crs(4326,inplace=True)
 print('Current CRS:{}'.format(gdf.crs))
 from geopy.distance import geodesic
 print('Distance between the two points is:{}'.format(geodesic(gdf.loc[0]['geometry'].coords,gdf.loc[1]['geometry'].coords).miles))
+
 
 
 @task(retries=3)
